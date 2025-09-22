8df87215ab838a7e1cc6cc1f4a2f169a9ac7918d
elnur <atakishiyev.elnur@gmail.com>
2025-09-22 20:28:06 +0400

    Initial Commit

---
+++ sekretess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sekretess
+//
+//  Created by Elnur Atakishiyev on 22.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sekretessTests/sekretessTests.swift
@@ -0,0 +1,17 @@
+//
+//  sekretessTests.swift
+//  sekretessTests
+//
+//  Created by Elnur Atakishiyev on 22.09.25.
+//
+
+import Testing
+@testable import sekretess
+
+struct sekretessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
